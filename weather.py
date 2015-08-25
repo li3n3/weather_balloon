078cbef4c5d5b1f86f25e4bc2784b5465929339d
Liene Verzemnieks <liene@simple.com>
2015-08-25 18:24:44 -0400

    add some more detail, destroy functionality with new argparse scaffolding

--- weather.py
+++ weather.py
@@ -1,10 +1,27 @@
 import urllib
 import json
 import os
+import argparse
 
+# get the Dark Sky API key in a cool safe way
 apikey = os.environ['DARKSKY_API_KEY']
-recursecenter = {'lat': '40.72078', 'lon': '-74.001119'}
-# defint the base URL for API calls
+
+# To start, let's grab whatever arguments were passed in.
+# First, we make a parser with argparse:
+parser = argparse.ArgumentParser(description = 'Specify optional preferences.')
+# Next, we'll define what options we'd like to, um, define
+parser.add_argument('-location', '-l', help = 'specify a particular location')
+
+# Put all those arguments into something we can use!
+args = parser.parse_args()
+
+
+# Let's define several locations that we may care about
+locations = {'recursecenter': {'lat': '40.72078', 'lon': '-74.001119'},
+             'portland_home': {'lat': '45.55902', 'lon': '-122.630664'},
+             'parents':       {'lat': '47.29085', 'lon': '-122.40482'}}
+
+# define the base URL for API calls
 url = 'https://api.forecast.io/forecast/APIKEY/LATITUDE,LONGITUDE'
 # put in the details we actually need to get real information
 s = url.replace('APIKEY', apikey).replace('LATITUDE', recursecenter['lat']).replace('LONGITUDE', recursecenter['lon'])
@@ -16,6 +33,8 @@ data = f.read()
 # turn this into a dictionary, by the power of JSON
 d = json.loads(data)
 
+minutely = d['minutely']
+print 'Imminently: {0}'.format(minutely['summary'])
 hourly = d['hourly']
-print hourly['summary']
+print 'Coming up: {0}'.format(hourly['summary'])
 
